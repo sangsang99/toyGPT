5178f16dec8629963f5067133644e0b20a733dfa
sangsang99 <idolphin2020@gmail.com>
2023-03-10 15:59:25 +0900

    UI다듬기 초안완성 Ver 1.0.0

--- main/blue/bluefile.py
+++ main/blue/bluefile.py
@@ -3,10 +3,8 @@ from flask import Blueprint, render_template
 
 var_blue = Blueprint("blue", __name__, template_folder="templates", url_prefix='/blue')
 
-@var_blue.route('/')
-def root():
-    return render_template('index.html')
+@var_blue.route('/', methods=['POST'])
+def root_prompt():
+    prompt = request.form['prompt']
+    return render_template('index.html', prompt=prompt)
 
-@var_blue.route('/<get>')
-def root_get(get):
-    return render_template('index.html', getValue=get)
