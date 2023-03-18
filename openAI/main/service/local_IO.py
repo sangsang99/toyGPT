43a1d22ca684f272c6dfca4c50c2ae8ad2b13de7
sangsang99 <idolphin2020@gmail.com>
2023-03-18 23:12:10 +0900

    git test run.py파일 확인

---
+++ openAI/main/service/local_IO.py
@@ -0,0 +1,72 @@
+import csv
+import os
+import requests
+from io import BytesIO
+from PIL import Image
+
+#csv저장 절대경로
+csv_path = 'D:\kukbee\openAI\csv\\'
+img_path = 'D:\kukbee\openAI\main\static\src\img\\'
+
+def save_to_csv(data, prompt):
+    """데이터를 입력받아 CSV 파일로 저장하는 함수"""
+    filename = csv_path + prompt + '.csv'
+
+    if not os.path.exists(filename):
+        with open(filename, 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['title', 'content', 'image_url', 'summary' ,'tag'])
+
+    with open(filename, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
+
+
+
+def save_url_img(img_url, prompt) :
+    # Send a GET request to the URL
+    response = requests.get(img_url)
+
+    # Open the image using Pillow
+    image = Image.open(BytesIO(response.content))
+
+    # Save the image to a file
+    filename = img_path + prompt + ".jpg"
+    image.save(filename)
+
+
+#csv 파일에서 데이터 읽어오기
+def read_csv(prompt) :
+    csv_file_name = csv_path + prompt + '.csv'
+    with open(csv_file_name, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
+            if i == 0:  # 첫 번째 줄인 경우 (header 정보가 없는 경우)
+                continue
+            else:  # 첫 번째 줄이 아닌 경우
+                title_col, content_col, image_col, summary_col, tags_col = 0, 1, 2, 3, 4  
+                
+            title, content, image_path, summary, tags = \
+                  row[title_col], row[content_col], row[image_col], row[summary_col], row[tags_col]
+
+            data = {
+                        'title': title,
+                        'content': content,
+                        'image_path': image_path,
+                        'summary': summary,
+                        'tags': tags  
+                    }          
+    return data
+
+def compile_tags(tags):
+
+    # Define the words to remove as a list
+    words_to_remove = ["'", "[", "]"]
+
+    # Remove the words using replace()
+    for word in words_to_remove:
+        tags = tags.replace(word, "")
+
+    tags = tags.split(",")
+
+    return tags
---
+++ openAI/main/service/openAI_input.py
@@ -0,0 +1,136 @@
+## import ===================================================================================
+import requests
+import json
+import random
+from main.service import key
+
+
+## 번역기 ===================================================================================
+
+def google_translate(text, target_language):
+    import six
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target_language)
+    return result['translatedText']
+
+## 글 가져오기 / 이미지 가져오기 / csv로 저장하기 ===================================================================================
+class prompt_factory:
+
+    def get_blog_content(api_key, prompt, length=2000):
+        """입력받은 prompt에 대한 블로그 제목과 글을 생성하는 함수"""
+        
+        prompt_opt = "I'm trying to write a blog post. \
+                    When I type in the prompt, \
+                    you treat it as a post title and write the body text in markdown format within 2,000 characters. \
+                    The next sentence is the prompt. "
+        
+        # Set up the translation client, target(ko - en)   
+        en_prompt = google_translate(prompt, 'en')
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {api_key}',
+        }
+
+        data = {
+            'model': 'text-davinci-002',
+            'prompt': f'{prompt_opt} + {en_prompt}',
+            'temperature': 0.8,
+            'max_tokens': length,
+        }
+
+        response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=data)
+        result = json.loads(response.text)
+        en_text = result['choices'][0]['text'].strip()
+
+        # Set up the translation client, target(ko - en)   
+        ko_text = google_translate(en_text, 'ko')
+
+        return ko_text
+    
+    def get_summary(api_key, result_content, length=250):
+        """prompt로 생성한 글을 요약해주는 함수"""
+        
+        prompt_opt = "Summarize the following content in two sentences or less. "
+
+        # Set up the translation client, target(ko - en)   
+        en_prompt = google_translate(result_content, 'en')
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {api_key}',
+        }
+
+        data = {
+            'model': 'text-davinci-002',
+            'prompt': f'{prompt_opt} + {en_prompt}',
+            'temperature': 0.8,
+            'max_tokens': length,
+        }
+
+        response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=data)
+        result = json.loads(response.text)
+        en_text = result['choices'][0]['text'].strip()
+        # Set up the translation client, target(ko - en)   
+        ko_text = google_translate(en_text, 'ko')
+
+        return ko_text
+
+    def get_tag(api_key, result_content, length=500):
+        """prompt로 생성한 글을 요약해주는 함수"""
+        
+        prompt_opt = "Generate 5 korean words close to following content. \
+                     and don't use '#' in front the word, connect each word with comma. "
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {api_key}',
+        }
+
+        data = {
+            'model': 'text-davinci-002',
+            'prompt': f'{prompt_opt} + {result_content}',
+            'temperature': 0.8,
+            'max_tokens': length,
+        }
+
+        response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=data)
+        result = json.loads(response.text)
+        tags = result['choices'][0]['text'].strip().split(',')
+        return tags
+
+
+    def get_image_url(api_key, query):
+        """입력받은 query에 대한 이미지 URL을 가져오는 함수"""
+        
+        # Set up the translation client, target(ko - en)
+        ko_query = query
+        en_query = google_translate(ko_query, 'en')
+
+        headers = {
+            'Accept-Version': 'v1',
+            'Authorization': f'Client-ID {api_key}',
+        }
+
+        params = (
+            ('query', en_query),
+            ('orientation', 'landscape'),
+            ('per_page', '30'),
+        )
+
+        response = requests.get('https://api.unsplash.com/search/photos', headers=headers, params=params)
+        results = json.loads(response.text)['results']
+        
+        if len(results) > 0:
+            result = random.choice(results)
+            return result['urls']['regular']
+        else:
+            return None
