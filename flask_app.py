ee88e575ecc1f0a6037da73c210b5f5ff173265b
sangsang99 <idolphin2020@gmail.com>
2023-03-22 11:29:44 +0900

    배포 test

--- flask_app.py
+++ flask_app.py
@@ -2,4 +2,4 @@ from main import app
 
 #최초 실행파일(?)은 __name__변수에 __main__이 설정되며 해당 서버가 유일함을 증명한다.
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5500)
+    app.run(host='0.0.0.0', debug=True, port=5000)
